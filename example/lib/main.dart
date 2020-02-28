25ac263b539b667cb7d85509f6d837772ff7de80
yohom <yohombao@qq.com>
2020-02-28 15:43:41 +0800

    feat: demo初步

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,10 +1,12 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:bmap_map_fluttify/bmap_map_fluttify.dart';
+import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final manager = await BMKMapManager.create__();
+  await manager.startGeneralDelegate('KryNE2jVLpf3tCYxpxnnc5wEjGyztvEf', null);
+  runApp(MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -12,34 +14,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await BmapMapFluttify.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,9 +21,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+        body: BMKMapView_iOS(),
       ),
     );
   }
--- lib/bmap_map_fluttify.dart
+++ lib/bmap_map_fluttify.dart
@@ -1,13 +1,2 @@
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class BmapMapFluttify {
-  static const MethodChannel _channel =
-      const MethodChannel('bmap_map_fluttify');
-
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-}
+export 'src/android/android.export.g.dart';
+export 'src/ios/ios.export.g.dart';
