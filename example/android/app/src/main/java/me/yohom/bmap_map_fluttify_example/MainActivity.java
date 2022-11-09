25f0f2f7000d74355ee6ee9e8ff7d1091b439462
yohom <yohombao@qq.com>
2022-11-09 19:42:32 +0800

    enhance: 去掉example工程

--- example/android/app/src/main/java/me/yohom/bmap_map_fluttify_example/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package me.yohom.bmap_map_fluttify_example;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- example/lib/utils/utils.export.dart
+++
@@ -1,2 +0,0 @@
-export 'misc.dart';
-export 'next_latlng.dart';
